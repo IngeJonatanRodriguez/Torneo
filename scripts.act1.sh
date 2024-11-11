d15e3d187c8faefce31509c737d449fe3af16ddc
JONATAN DAVID RODRIGUEZ RINCON <inge.jonatanrodriguez@gmail.com>
2024-11-11 13:13:06 -0500

    Torneo de Futbol

---
+++ scripts.act1.sh
@@ -0,0 +1,10 @@
+#! Crear base de datos
+use copaanthropic
+
+#! Crear colecciones
+db.createCollection("Jugadores")
+db.createCollection("equipos")
+db.createCollection("arbitros")
+db.createCollection("Partidos")
+db.createCollection("Resultados")
+db.createCollection("tablaDePosiciones")
